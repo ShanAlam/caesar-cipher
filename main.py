d24e469200f21c0df85ce8fca6404cbece43cbe4
shanalam <62662499+ShanAlam@users.noreply.github.com>
2022-10-29 16:44:47 +0000

    Completed Ceasar Cipher code.

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from art import logo
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def ceasar(text, shift, direction):
+  
+  ceasar_text = ""
+  
+  if shift > 26:
+    shift = shift%26
+    
+  if direction == "encrypt":
+    for letter in text:
+      if letter not in alphabet:
+        ceasar_text += letter
+      else:
+        position = alphabet.index(letter)
+        ceasar_text += alphabet[position + shift]
+    print(f"The encrypted text is {ceasar_text}")
 
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
+  elif direction == "decrypt":
+    for letter in text:
+      if letter not in alphabet:
+        ceasar_text += letter
+      else:
+        position = alphabet.index(letter)
+        ceasar_text += alphabet[position - shift]
+    print(f"The decrypted text is {ceasar_text}")
 
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+  else:
+    print("Invalid input, program terminated")
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+print(logo)
+
+termination = "no"
+
+while termination == "no":
+  
+  user_direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+  user_text = input("Type your message:\n").lower()
+  user_shift = int(input("Type the shift number:\n"))
+
+  ceasar(text=user_text, shift=user_shift, direction=user_direction)
+  
+  termination = input("Type 'yes' if you want the program to terminate. Otherwise type 'no':\n").lower()
+  
+    
+    
+
+
